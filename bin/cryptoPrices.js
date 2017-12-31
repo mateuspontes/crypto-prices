e8bad2b3356b24c250932e287e876bf6306bb5ae
Mateus Pontes <mateus.pontes@corp.globo.com>
2017-12-31 15:53:06 -0200

    Get prices

---
+++ bin/cryptoPrices.js
@@ -0,0 +1,45 @@
+const chalk = require("chalk");
+const request = require("request");
+const ora = require("ora");
+
+const spinner = ora({
+  text: "Retrieving Crypto prices...",
+  color: "yellow"
+});
+
+function cryptoPrices(currency = null, limit = 30) {
+  let url = `https://api.coinmarketcap.com/v1/ticker`;
+  if (currency) {
+    url += `/${currency}`;
+  }
+  url += `?limit=${limit}`;
+
+  spinner.start();
+
+  request(url, (error, response, body) => {
+    let apiResponse;
+    spinner.stop();
+
+    try {
+      apiResponse = JSON.parse(body);
+    } catch (parseError) {
+      console.log(chalk.red("Something went wrong in the API. Try in a few minutes."));
+      return parseError;
+    }
+
+    apiResponse.forEach(crypto => {
+      percent_1h = crypto.percent_change_1h;
+      percent_24h = crypto.percent_change_24h;
+      percent_7d = crypto.percent_change_7d;
+
+      console.log(` * ${chalk.cyan(crypto.name)} ${crypto.name.length <= 10 ? ` \t` : ``} \t \
+${chalk.yellow("$ " + crypto.price_usd)} \
+\t ${chalk.gray("|")} \t  1h: ${percent_1h >= 0 ? chalk.green(percent_1h + "%") : chalk.red(percent_1h + "%")} \
+\t ${chalk.gray("|")} \t 24h: ${percent_24h >= 0 ? chalk.green(percent_24h + "%") : chalk.red(percent_24h + "%")} \
+\t ${chalk.gray("|")} \t  7d: ${percent_7d >= 0 ? chalk.green(percent_7d + "%") : chalk.red(percent_7d + "%")} \
+`);
+    });
+  });
+}
+
+module.exports = cryptoPrices;
---
+++ bin/main.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+
+const program = require('commander');
+const cryptoPrices = require("./cryptoPrices");
+
+program.description('Get crypto prices to any currency defined.').option('-c, --currency <currency>', 'Currency to be fetched.').option('-l, --limit <limit>', 'How many coins to be returned.').parse(process.argv);
+
+cryptoPrices(program.currency, program.limit);
